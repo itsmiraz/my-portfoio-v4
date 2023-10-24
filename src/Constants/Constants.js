6e8718ac7f7502b46c4cbcecea3a3341b003e26e
unknown <mirajulislamofficial044@gmail.com>
2023-10-24 15:38:39 +0600

    new tech-img-added

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -2,6 +2,156 @@ import resdium from "../../public/Assets/projects/resdium.webp";
 import shopNow from "../../public/Assets/projects/shop-now.webp";
 import expensetracker from "../../public/Assets/projects/expense-tracker.webp";
 
+// Front End Techs
+import vitejs from '../../public/Assets/Front-end/vitejs.webp'
+import bootstrap from '../../public/Assets/Front-end/bootstrap.webp'
+import css3 from '../../public/Assets/Front-end/css3.webp'
+import framermotion from '../../public/Assets/Front-end/framer-motion.webp'
+import html5 from '../../public/Assets/Front-end/html5.webp'
+import react from '../../public/Assets/Front-end/react.webp'
+import javascript from '../../public/Assets/Front-end/javascript.webp'
+import nexjts from '../../public/Assets/Front-end/nexjts.webp'
+import redux from '../../public/Assets/Front-end/redux.webp'
+import tailwind from '../../public/Assets/Front-end/tailwind.webp'
+import tanstack from '../../public/Assets/Front-end/tanstack-query.webp'
+import router from '../../public/Assets/Front-end/react-router.webp'
+
+//Backend Techs
+import nodejs from '../../public/Assets/Backend/nodejs.webp'
+import mongodb from '../../public/Assets/Backend/mongodb.webp'
+import express from '../../public/Assets/Backend/express.webp'
+import jwt from '../../public/Assets/Backend/jwt.webp'
+
+
+// Dev Ops
+import nginx from '../../public/Assets/dev-ops/nginx.webp'
+import pm2 from '../../public/Assets/dev-ops/pm2.webp'
+import ubontu from '../../public/Assets/dev-ops/ubontu.webp'
+
+// Others
+import figma from '../../public/Assets/others/figma.webp'
+import photoshop from '../../public/Assets/others/photoshop.webp'
+
+// Auth
+import firebase from '../../public/Assets/auth/firebase.webp'
+import auth0 from '../../public/Assets/auth/auth0.webp'
+
+
+export const Auth = [
+  
+  {
+    img: firebase,
+    url:'https://firebase.google.com/'
+  },
+  
+  {
+    img: auth0,
+    url:'https://auth0.com/'
+  },
+]
+
+export const othersTech = [
+  {
+    img: figma,
+    url:'https://www.figma.com/'
+  },
+  {
+    img: photoshop,
+    url:'https://www.adobe.com/products/photoshop.html'
+  },
+]
+
+export const DevOps = [
+  
+    {
+      img: nginx,
+      url:'https://www.nginx.com/'
+    },
+    {
+      img: pm2,
+      url:'https://pm2.keymetrics.io/'
+    },
+    {
+      img: ubontu,
+      url:'https://ubuntu.com/'
+    },
+  
+]
+
+export const FrontEndTechs = [
+  {
+    img: nexjts,
+    url:'https://nextjs.org/'
+  },
+  {
+    img: redux,
+    url:'https://redux.js.org/'
+  },
+  {
+    img: tailwind,
+    url:'https://tailwindcss.com/'
+  },
+  {
+    img: tanstack,
+    url:'https://tanstack.com/query/latest'
+  },
+  {
+    img: vitejs,
+    url:'https://vitejs.dev/'
+  },
+  {
+    img: bootstrap,
+    url:'https://getbootstrap.com/'
+  },
+  {
+    img: css3,
+    url:'https://www.w3schools.com/css/'
+  },
+  {
+    img: router,
+    url:'https://reactrouter.com/en/main'
+  },
+  {
+    img: framermotion,
+    url:'https://www.framer.com/motion/'
+  },
+  {
+    img: html5,
+    url:'https://www.w3schools.com/html/'
+  },
+  {
+    img: react,
+    url:'https://react.dev/'
+  },
+  {
+    img: javascript,
+    url:'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+  },
+  
+  
+]
+export const BackedEndTechs = [
+  {
+    img: nodejs,
+    url:'https://nodejs.org/en'
+  },
+  {
+    img: mongodb,
+    url:'https://www.mongodb.com/'
+  },
+  {
+    img: express,
+    url:'https://expressjs.com/'
+  },
+  {
+    img: jwt,
+    url:'https://jwt.io/'
+  },
+  
+  
+  
+]
+
 export const ProjectsData = [
   {
     img: resdium,
@@ -47,3 +197,6 @@ export const ExperienceData = [
     location: "Bangladesh",
   },
 ];
+
+
+
