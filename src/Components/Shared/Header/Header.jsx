93fe16b93a43b0022341ee467df4337ab6545dc5
unknown <mirajulislamofficial044@gmail.com>
2023-10-24 21:38:47 +0600

    header padding changed

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
             initial={{ opacity: 0, y: -100, }}
             animate={{ opacity: 1, y: 0, }}
             transition={{ duration: 0.4 }}
-            className='p-6 sticky top-0 z-50'>
+            className='p-4 md:p-6 sticky top-0 z-50'>
             <header className='flex justify-between items-center bg-[#001233bb] backdrop-blur-lg p-3 md:p-6 rounded '>
                 <Image className='max-w-[50px] md:max-w-[80px] lg:max-w-[100px]' src={logo} alt='miraj-logo' />
                 <div>
