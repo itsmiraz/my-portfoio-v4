e7ce2eca9d4cdc712759d26be507792abd7858c8
unknown <mirajulislamofficial044@gmail.com>
2023-10-24 21:14:30 +0600

    new change

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,41 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MIRAJ HOSSEN PORTFOLIO | WEB DEVELOPER</title>
+        {" "}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MIRAJ HOSSEN | PORTFOLIO</title>
+        <meta
+          name="description"
+          content="Welcome to my portfolio! I'm Miraj Hossen, a full-stack developer from Bangladesh, passionate about creating web applications and digital experiences that stand out."
+        />
+        <meta
+          name="keywords"
+          content="Miraj Hossen, full-stack developer, web development, portfolio, web applications, digital experiences, Bangladesh, coding, web developer, programming, innovative websites, user-friendly, tech enthusiast"
+        />
+        <meta property="og:site_name" content="Miraj Hossen" />
+        <meta property="og:title" content="Miraj Hossen | Portfolio" />
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio! I'm Miraj Hossen, a full-stack developer from Bangladesh, passionate about creating web applications and digital experiences that stand out."
+        />
+        {/* <meta
+          property="og:image"
+          content="https://your-portfolio-image-url.com/your-image.jpg"
+        /> */}
+        <meta
+          property="og:url"
+          content="https://mirajhossen.netlify.app/"
+        />
+        <meta property="og:type" content="website" />
+        <link rel="canonical" href="https://mirajhossen.netlify.app/" />
       </Head>
 
-       <Hero />
-     <AboutMe />
-     <Projects />
-    <Experience />
-         <SkillSet />
-      <Contact/> 
+      <Hero />
+      <AboutMe />
+      <Projects />
+      <Experience />
+      <SkillSet />
+      <Contact />
     </>
   );
 }
